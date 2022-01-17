d3af6b5cd12c281a45d0e9ce79e6b7979cc3cd3c
uus02030@naver.com <uus02030@naver.com>
2022-01-17 16:19:08 +0900

    refer

--- deepwalk/__main__.py
+++ deepwalk/__main__.py
@@ -1,31 +1,25 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import stellargraph as sg
-import pandas as pd
-from sklearn.manifold import TSNE
-
 import os
 import sys
 import random
 from io import open
 from argparse import ArgumentParser, FileType, ArgumentDefaultsHelpFormatter
-from collections import Counter
-from concurrent.futures import ProcessPoolExecutor
 import logging
-
-from deepwalk import graph
-from deepwalk import walks as serialized_walks
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+import networkx as nx
+import graph
+import walks as serialized_walks
 from gensim.models import Word2Vec
-from skipgram import Skipgram
-
-from six import text_type as unicode
-from six import iteritems
+import numpy as np
 from six.moves import range
 
-import psutil
-from multiprocessing import cpu_count
+import matplotlib.pyplot as plt
 
+import psutil
+import pandas as pd
+from multiprocessing import cpu_count
 
 p = psutil.Process(os.getpid())
 try:
@@ -39,9 +33,7 @@ except AttributeError:
 logger = logging.getLogger(__name__)
 LOGFORMAT = "%(asctime).19s %(levelname)s %(filename)s: %(lineno)s %(message)s"
 
-
 def debug(type_, value, tb):
-    
     if hasattr(sys, 'ps1') or not sys.stderr.isatty():
         sys.__excepthook__(type_, value, tb)
     else:
@@ -51,107 +43,171 @@ def debug(type_, value, tb):
         print(u"\n")
         pdb.pm()
 
+##수정
+node_class = dict()
+def read_and_make_graph_nx_version(args):
+    global node_class
+    node_class = dict()
+    edgelist = list()
+    class_num = 1
+    class_name_to_num = dict()
+    with open(args.input + os.sep + 'cora.content', 'r') as f, open(args.input + os.sep + 'cora.cites', 'r') as f2:
+        for line in f:
+            l = line.strip().split()
+            class_name = l[-1]
+            if class_name not in class_name_to_num:
+                class_name_to_num[class_name] = class_num
+                class_num += 1
+            node_class[l[0]] = class_name  # class_name_to_num[class_name]
+            for line in f2:
+                l = line.strip().split()
+                edgelist.append((l[1], l[0]))
+
+    nx_G = nx.DiGraph()
+    nx_G.add_edges_from(edgelist)
+    deepwalk_G = graph.from_networkx(nx_G, undirected=False)
+    return nx_G, deepwalk_G
+
+##수정
 def process(args):
-
-    if args.format == "adjlist":
-        G = graph.load_adjacencylist(args.input, undirected = args.undirected)
-    elif args.format == "edgelist":
-        G = graph.load_edgelist(args.input, undirected = args.undirected)
-    elif args.format == "mat":
-        G = graph.load_matfile(args.input, variable_name = args.matfile_variable_name, undirected = args.undirected)
-    else:
-        raise Exception("Unknown file format: '%s'.  Valid formats: 'adjlist', 'edgelist', 'mat'" % args.format)
-
+    global node_class
+    nx_G, G = read_and_make_graph_nx_version(args)
     print("Number of nodes: {}".format(len(G.nodes())))
-
     num_walks = len(G.nodes()) * args.number_walks
-
     print("Number of walks: {}".format(num_walks))
-
     data_size = num_walks * args.walk_length
-
-    print("Data size (walks * length): {}".format(data_size))
-
-    if data_size < args.max_memory_data_size:
+    print("Data size (walks*length): {}".format(data_size))
+    if data_size < args.max_memory_data_size:  # 메모리에 data가 올라갈 수 있으면
         print("Walking...")
-        walks = graph.build_deepwalk_corpus(G, num_paths = args.number_walks, path_length = args.walk_length, alpha = 0, rand = random.Random(args.seed))
+        walks = graph.build_deepwalk_corpus(G, num_paths=args.number_walks,
+                                            path_length=args.walk_length, alpha=0, rand=random.Random(args.seed))
         print("Training...")
-        model = Word2Vec(walks, vector_size= args.representation_size, window = args.window_size, min_count = 0, workers = args.workers)
+        model = Word2Vec(walks, vector_size=args.representation_size, window=args.window_size, min_count=0, sg=1, hs=1,
+                         workers=args.workers)
+        model.wv.save_word2vec_format(args.output)
     else:
-        print("Data size {} is larger than limit (max-memory-data-size: {}). Dumping walks to disk.".format(data_size, args.max_memory_data_size))
+        print("Data size {} is larger than limit (max-memory-data-size: {}).  Dumping walks to disk.".format(data_size,
+                                                                                                             args.max_memory_data_size))
         print("Walking...")
-
         walks_filebase = args.output + ".walks"
-        walk_files = serialized_walks.write_walks_to_disk(G, walks_filebase, num_paths = args.number_walks, path_length = args.walk_length, alpha = 0, rand = random.Random(args.seed), num_workers = args.workers)
+        walk_files = serialized_walks.write_walks_to_disk(G, walks_filebase, num_paths=args.number_walks,
+                                                          path_length=args.walk_length, alpha=0,
+                                                          rand=random.Random(args.seed),
+                                                          num_workers=args.workers)
 
-        print("Counting vertex frequency...")
-        if not args.vertex_freq_degree:
-            vertex_counts = serialized_walks.count_textfiles(walk_files, args.workers)
-        else:
-            # use degree distribution for frequency in tree
-            vertex_counts = G.degree(nodes = G.iterkeys())
+    ##참고
+    node_classification(model, nx_G)
+    tsne_visualization(model)
 
-        print("Training...")
-        model = Skipgram(sentences = serialized_walks.combine_files_iter(walk_files), vocabulary_counts = vertex_counts, size = args.representation_size, window = args.window_size, min_count = 0, workers = args.workers)
+##수정
+def tsne_visualization(model):
+    global node_class
+    node_ids = model.wv.index_to_key  # list of node IDs
+    node_subjects = pd.Series(node_class)
+    node_targets = node_subjects.loc[node_ids]
 
-    model.wv.save_word2vec_format(args.output)
+    transform = TSNE  # PCA
+    trans = transform(n_components=3)
+    node_embeddings_3d = trans.fit_transform(model.wv.vectors)
 
+    alpha = 0.7
+    label_map = {l: i for i, l in enumerate(np.unique(node_targets))}
+    node_colours = [label_map[target] for target in node_targets]
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    # plt.axes().set(aspect="equal")
+    ax.scatter(
+        node_embeddings_3d[:, 0],
+        node_embeddings_3d[:, 1],
+        node_embeddings_3d[:, 2],
+        c=node_colours,
+        cmap="jet",
+        alpha=alpha,
+    )
+    plt.title("{} visualization of node embeddings".format(transform.__name__))
+    plt.show()
+    plt.savefig("visualization.png")
+
+##수정
+def node_classification(model, nx_G):
+    K = 7
+    kmeans = KMeans(n_clusters=K, random_state=0)
+    kmeans.fit(model.wv.vectors)
+    print(model.wv.vectors)
+
+    for n, label in zip(model.wv.index_to_key, kmeans.labels_):
+        nx_G.nodes[n]['label'] = label
+
+    for n in nx_G.nodes(data=True):
+        if 'label' not in n[1].keys():
+            n[1]['label'] = 7
+    plt.figure(figsize=(12, 6), dpi=600)
+    nx.draw_networkx(nx_G, pos=nx.layout.spring_layout(nx_G),
+                     node_color=[[n[1]['label'] for n in nx_G.nodes(data=True)]],
+                     cmap=plt.cm.rainbow,
+                     node_shape='.',
+                     font_size='2'
+                     )
+
+    plt.axis('off')
+    plt.savefig('img.png', bbox_inches='tight', pad_inches=0)
 
 def main():
-    parser = ArgumentParser("deepwalk", formatter_class = ArgumentDefaultsHelpFormatter, conflict_handler = 'resolve')
+    parser = ArgumentParser("deepwalk",
+                            formatter_class=ArgumentDefaultsHelpFormatter,
+                            conflict_handler='resolve')
 
-    parser.add_argument("--debug", dest = "debug", action = 'store_true', default = False, 
-                        help = "drop a debugger if an exception is raised.")
+    parser.add_argument("--debug", dest="debug", action='store_true', default=False,
+                        help="drop a debugger if an exception is raised.")
 
-    parser.add_argument('--format', default = 'adjlist', 
-                        help = 'File format of input file')
+    parser.add_argument('--format', default='adjlist',
+                        help='File format of input file')
 
-    parser.add_argument('--input', nargs = '?', required = True, 
-                        help = 'Input graph file')
+    parser.add_argument('--input', nargs='?', required=True,
+                        help='Input graph file')
 
-    parser.add_argument("-l", "--log", dest = "log", default = "INFO", 
-                        help = "log verbosity level")
+    parser.add_argument("-l", "--log", dest="log", default="INFO",
+                        help="log verbosity level")
 
-    parser.add_argument('--matfile-variable-name', default = 'network', 
-                        help = 'variable name of adjacency matrix inside a .mat file.')
+    parser.add_argument('--matfile-variable-name', default='network',
+                        help='variable name of adjacency matrix inside a .mat file.')
 
-    parser.add_argument('--max-memory-data-size', default = 1000000000, type = int, 
-                        help = 'Size to start dumping walks to disk, ' 
-                               'instead of keeping them in memory.')
+    parser.add_argument('--max-memory-data-size', default=1000000000, type=int,
+                        help='Size to start dumping walks to disk, instead of keeping them in memory.')
 
-    parser.add_argument('--number-walks', default = 10, type = int, 
-                        help = 'Number of random walks to start at each node')
+    parser.add_argument('--number-walks', default=10, type=int,
+                        help='Number of random walks to start at each node')
 
-    parser.add_argument('--output', required = True, 
-                        help = 'Output representation file')
+    parser.add_argument('--output', required=True,
+                        help='Output representation file')
 
-    parser.add_argument('--representation-size', default = 64, type = int, 
-                        help = 'Number of latent dimensions to learn for each node.')
+    parser.add_argument('--representation-size', default=64, type=int,
+                        help='Number of latent dimensions to learn for each node.')
 
-    parser.add_argument('--seed', default = 0, type = int,
-                        help = 'Seed for random walk generator.')
+    parser.add_argument('--seed', default=0, type=int,
+                        help='Seed for random walk generator.')
 
-    parser.add_argument('--undirected', default = True, type = bool, 
-                        help = 'Treat graph as undirected.')
+    parser.add_argument('--undirected', default=True, type=bool,
+                        help='Treat graph as undirected.')
 
-    parser.add_argument('--vertex-freq-degree', default = False, action = 'store_true',
-                        help = 'Use vertex degree to estimate the frequency of nodes '
-                           'in the random walks. This option is faster than '
-                           'calculating the vocabulary.')
+    parser.add_argument('--vertex-freq-degree', default=False, action='store_true',
+                        help='Use vertex degree to estimate the frequency of nodes '
+                             'in the random walks. This option is faster than '
+                             'calculating the vocabulary.')
 
-    parser.add_argument('--walk-length', default = 40, type = int,
-                        help = 'Length of the random walk started at each node')
+    parser.add_argument('--walk-length', default=40, type=int,
+                        help='Length of the random walk started at each node')
 
-    parser.add_argument('--window-size', default = 5, type = int,
-                        help = 'Window size of skipgram model.')
+    parser.add_argument('--window-size', default=5, type=int,
+                        help='Window size of skipgram model.')
 
-    parser.add_argument('--workers', default = 1, type = int,
-                        help = 'Number of parallel processes.')
-
+    parser.add_argument('--workers', default=1, type=int,
+                        help='Number of parallel processes.')
 
     args = parser.parse_args()
     numeric_level = getattr(logging, args.log.upper(), None)
-    logging.basicConfig(format = LOGFORMAT)
+    logging.basicConfig(format=LOGFORMAT)
     logger.setLevel(numeric_level)
 
     if args.debug:
--- deepwalk/graph.py
+++ deepwalk/graph.py
@@ -1,40 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 """Graph utilities."""
-
 import logging
-import sys
 from io import open
-from os import path
 from time import time
-from glob import glob
 from six.moves import range, zip, zip_longest
 from six import iterkeys
 from _collections_abc import Iterable
 from collections import defaultdict
-from multiprocessing import cpu_count
 import random
-from random import shuffle
-from itertools import product,permutations
+from itertools import product, permutations
 from scipy.io import loadmat
 from scipy.sparse import issparse
 
-from concurrent.futures import ProcessPoolExecutor
-
-from multiprocessing import Pool
-from multiprocessing import cpu_count
-
 logger = logging.getLogger("deepwalk")
-
 
 __author__ = "Bryan Perozzi"
 __email__ = "bperozzi@cs.stonybrook.edu"
 
 LOGFORMAT = "%(asctime).19s %(levelname)s %(filename)s: %(lineno)s %(message)s"
 
+
 class Graph(defaultdict):
-    """Efficient basic implementation of nx `Graph' â€“ Undirected graphs with self loops"""  
+    """Efficient basic implementation of nx `Graph' â€“ Undirected graphs with self loops"""
+
     def __init__(self):
         super(Graph, self).__init__(list)
 
@@ -44,39 +33,37 @@ class Graph(defaultdict):
     def adjacency_iter(self):
         return self.iteritems()
 
-    def subgraph(self, nodes = {}):
+    def subgraph(self, nodes={}):
         subgraph = Graph()
-    
+
         for n in nodes:
             if n in self:
                 subgraph[n] = [x for x in self[n] if x in nodes]
-        
+
         return subgraph
 
     def make_undirected(self):
-  
+
         t0 = time()
 
-        for v in self.keys():
+        for v in list(self):
             for other in self[v]:
                 if v != other:
                     self[other].append(v)
-    
+
         t1 = time()
-        logger.info('make_directed: added missing edges {}s'.format(t1-t0))
+        logger.info('make_directed: added missing edges {}s'.format(t1 - t0))
 
         self.make_consistent()
         return self
 
     def make_consistent(self):
-        """ Each node's neighbours are now sorted and self loops are eliminated. """
-        
         t0 = time()
         for k in iterkeys(self):
             self[k] = list(sorted(set(self[k])))
-    
+
         t1 = time()
-        logger.info('make_consistent: made consistent in {}s'.format(t1-t0))
+        logger.info('make_consistent: made consistent in {}s'.format(t1 - t0))
 
         self.remove_self_loops()
 
@@ -88,33 +75,29 @@ class Graph(defaultdict):
         t0 = time()
 
         for x in self:
-            if x in self[x]: 
+            if x in self[x]:
                 self[x].remove(x)
                 removed += 1
-    
+
         t1 = time()
 
-        logger.info('remove_self_loops: removed {} loops in {}s'.format(removed, (t1-t0)))
+        logger.info('remove_self_loops: removed {} loops in {}s'.format(removed, (t1 - t0)))
         return self
 
     def check_self_loops(self):
-    
         for x in self:
             for y in self[x]:
                 if x == y:
                     return True
-    
+
         return False
 
     def has_edge(self, v1, v2):
-      
         if v2 in self[v1] or v1 in self[v2]:
             return True
-    
         return False
 
-    def degree(self, nodes = None):
-      
+    def degree(self, nodes=None):
         if isinstance(nodes, Iterable):
             return {v: len(self[v]) for v in nodes}
         else:
@@ -122,23 +105,20 @@ class Graph(defaultdict):
 
     def order(self):
         "Returns the number of nodes in the graph"
-    
-        return len(self)    
+        return len(self)
 
     def number_of_edges(self):
-        "Returns the number of edges in the graph"
-      
+        "Returns the number of nodes in the graph"
         return sum([self.degree(x) for x in self.keys()]) / 2
 
     def number_of_nodes(self):
         "Returns the number of nodes in the graph"
-    
-        return order()
+        return self.order()
 
-    def random_walk(self, path_length, alpha = 0, rand = random.Random(), start = None):
+    def random_walk(self, path_length, alpha=0, rand=random.Random(), start=None):
         """ Returns a truncated random walk.
-  
-            path_length: max length of the random walk.
+
+            path_length: Length of the random walk.
             alpha: probability of restarts.
             start: the start node of the random walk.
         """
@@ -147,7 +127,7 @@ class Graph(defaultdict):
             path = [start]
         else:
             # Sampling is uniform w.r.t V, and not w.r.t E
-            path = [rand.choice(G.keys())]
+            path = [rand.choice(list(G.keys()))]
 
         while len(path) < path_length:
             cur = path[-1]
@@ -158,29 +138,27 @@ class Graph(defaultdict):
                     path.append(path[0])
             else:
                 break
-              
-        return path
+        return [str(node) for node in path]
+
 
 # TODO add build_walks in here
 
-def build_deepwalk_corpus(G, num_paths, path_length, alpha = 0, rand = random.Random(0)):
-    
+def build_deepwalk_corpus(G, num_paths, path_length, alpha=0,
+                          rand=random.Random(0)):
     walks = []
 
     nodes = list(G.nodes())
-  
-    # generate num_paths random walks
+
     for cnt in range(num_paths):
-        # note how random choice is implemented
-        # first shuffle the list and traverse orderly the list
         rand.shuffle(nodes)
         for node in nodes:
-            walks.append(G.random_walk(path_length, rand = rand, alpha = alpha, start = node))
-  
+            walks.append(G.random_walk(path_length, rand=rand, alpha=alpha, start=node))
+
     return walks
 
-def build_deepwalk_corpus_iter(G, num_paths, path_length, alpha = 0, rand = random.Random(0)):
-    
+
+def build_deepwalk_corpus_iter(G, num_paths, path_length, alpha=0,
+                               rand=random.Random(0)):
     walks = []
 
     nodes = list(G.nodes())
@@ -188,41 +166,41 @@ def build_deepwalk_corpus_iter(G, num_paths, path_length, alpha = 0, rand = rand
     for cnt in range(num_paths):
         rand.shuffle(nodes)
         for node in nodes:
-            yield G.random_walk(path_length, rand = rand, alpha = alpha, start = node)
+            yield G.random_walk(path_length, rand=rand, alpha=alpha, start=node)
 
 
 def clique(size):
     return from_adjlist(permutations(range(1, size + 1)))
 
+
 # http://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks-in-python
-def grouper(n, iterable, padvalue = None):
-    "E.g. grouper(3, 'abcdefg', 'x') --> ('a','b','c'), ('d','e','f'), ('g','x','x')"
-    return zip_longest(*[iter(iterable)] * n, fillvalue = padvalue)
+def grouper(n, iterable, padvalue=None):
+    "grouper(3, 'abcdefg', 'x') --> ('a','b','c'), ('d','e','f'), ('g','x','x')"
+    return zip_longest(*[iter(iterable)] * n, fillvalue=padvalue)
+
 
 def parse_adjacencylist(f):
     adjlist = []
     for l in f:
-        # for each line in a file
         if l and l[0] != "#":
             introw = [int(x) for x in l.strip().split()]
             row = [introw[0]]
-            # list.extend extends a list by appendding elements from a iterable
             row.extend(set(sorted(introw[1:])))
             adjlist.extend([row])
-  
+
     return adjlist
 
+
 def parse_adjacencylist_unchecked(f):
-    
     adjlist = []
     for l in f:
         if l and l[0] != "#":
             adjlist.extend([[int(x) for x in l.strip().split()]])
-  
+
     return adjlist
 
-def load_adjacencylist(file_, undirected = False, chunksize = 10000, unchecked = True):
 
+def load_adjacencylist(file_, undirected=False, chunksize=10000, unchecked=True):
     if unchecked:
         parse_func = parse_adjacencylist_unchecked
         convert_func = from_adjlist_unchecked
@@ -234,36 +212,32 @@ def load_adjacencylist(file_, undirected = False, chunksize = 10000, unchecked =
 
     t0 = time()
 
+    total = 0
     with open(file_) as f:
-        with ProcessPoolExecutor(max_workers = cpu_count()) as executor:
-            total = 0 
-            for idx, adj_chunk in enumerate(executor.map(parse_func, grouper(int(chunksize), f))):
-                # map applies the function on each of the item
-                # thr grouper function returns each time n lines 
-                adjlist.extend(adj_chunk)
-                total += len(adj_chunk)
-  
+        for idx, adj_chunk in enumerate(map(parse_func, grouper(int(chunksize), f))):
+            adjlist.extend(adj_chunk)
+            total += len(adj_chunk)
+
     t1 = time()
 
-    logger.info('Parsed {} edges with {} chunks in {}s'.format(total, idx, t1-t0))
+    logger.info('Parsed {} edges with {} chunks in {}s'.format(total, idx, t1 - t0))
 
     t0 = time()
     G = convert_func(adjlist)
     t1 = time()
 
-    logger.info('Converted edges to graph in {}s'.format(t1-t0))
+    logger.info('Converted edges to graph in {}s'.format(t1 - t0))
 
     if undirected:
         t0 = time()
         G = G.make_undirected()
         t1 = time()
-        logger.info('Made graph undirected in {}s'.format(t1-t0))
+        logger.info('Made graph undirected in {}s'.format(t1 - t0))
 
-    return G 
+    return G
 
 
-def load_edgelist(file_, undirected = True):
-    
+def load_edgelist(file_, undirected=True):
     G = Graph()
     with open(file_) as f:
         for l in f:
@@ -273,24 +247,22 @@ def load_edgelist(file_, undirected = True):
             G[x].append(y)
             if undirected:
                 G[y].append(x)
-  
+
     G.make_consistent()
     return G
 
 
-def load_matfile(file_, variable_name = "network", undirected = True):
-    
+def load_matfile(file_, variable_name="network", undirected=True):
     mat_varables = loadmat(file_)
     mat_matrix = mat_varables[variable_name]
 
     return from_numpy(mat_matrix, undirected)
 
 
-def from_networkx(G_input, undirected = True):
-    
+def from_networkx(G_input, undirected=True):
     G = Graph()
 
-    for idx, x in enumerate(G_input.nodes_iter()):
+    for idx, x in enumerate(G_input.nodes()):
         for y in iterkeys(G_input[x]):
             G[x].append(y)
 
@@ -300,16 +272,15 @@ def from_networkx(G_input, undirected = True):
     return G
 
 
-def from_numpy(x, undirected = True):
-    
+def from_numpy(x, undirected=True):
     G = Graph()
 
     if issparse(x):
         cx = x.tocoo()
-        for i,j,v in zip(cx.row, cx.col, cx.data):
+        for i, j, v in zip(cx.row, cx.col, cx.data):
             G[i].append(j)
     else:
-      raise Exception("Dense matrices not yet supported.")
+        raise Exception("Dense matrices not yet supported.")
 
     if undirected:
         G.make_undirected()
@@ -319,9 +290,8 @@ def from_numpy(x, undirected = True):
 
 
 def from_adjlist(adjlist):
-
     G = Graph()
-    
+
     for row in adjlist:
         node = row[0]
         neighbors = row[1:]
@@ -331,14 +301,11 @@ def from_adjlist(adjlist):
 
 
 def from_adjlist_unchecked(adjlist):
-    
     G = Graph()
-    
+
     for row in adjlist:
         node = row[0]
         neighbors = row[1:]
         G[node] = neighbors
 
     return G
-
-
